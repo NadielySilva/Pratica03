6a6f3287997b3b7e0f6243d8b66fc49947c899d4
NadielySilva <nady.ceng@gmail.com>
2022-01-27 16:34:12 -0300

    First commit
    from GH dsktp

---
+++ script.js
@@ -0,0 +1,28 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+for(let i=1; i<=10;i++){
+    console.log(i)
+}
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+let n = 10;
+
+while(n>0){
+    console.log(n)
+    n--
+}
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+let odd = 1;
+
+while(odd<100){
+    console.log(odd)
+    odd+=2
+}
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+let even = 0;
+
+while(even<=100){
+    console.log(even)
+    even+=2
+}
